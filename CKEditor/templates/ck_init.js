e1bf568704890c2566684eb9c7bf4bfa5c71ebea
Zero King <l2dy@icloud.com>
2022-04-04 06:02:26 +0000

    Format code

--- CKEditor/templates/ck_init.js
+++ CKEditor/templates/ck_init.js
@@ -1,48 +1,48 @@
 $(document).ready(() => {
-  ClassicEditor
-    .create(document.querySelector('#text'), {
-      codeBlock: {
-        languages: [
-          { language: 'plaintext', label: 'Plain text' },
-          { language: 'bash', label: 'Bash' },
-          { language: 'c', label: 'C' },
-          { language: 'console', label: 'Shell session' },
-          { language: 'cs', label: 'C#' },
-          { language: 'cpp', label: 'C++' },
-          { language: 'css', label: 'CSS' },
-          { language: 'diff', label: 'Diff' },
-          { language: 'elixir', label: 'Elixir' },
-          { language: 'erlang', label: 'Erlang' },
-          { language: 'go', label: 'Go' },
-          { language: 'html', label: 'HTML' },
-          { language: 'java', label: 'Java' },
-          { language: 'javascript', label: 'JavaScript' },
-          { language: 'json', label: 'JSON' },
-          { language: 'kotlin', label: 'Kotlin' },
-          { language: 'php', label: 'PHP' },
-          { language: 'python', label: 'Python' },
-          { language: 'ruby', label: 'Ruby' },
-          { language: 'typescript', label: 'TypeScript' },
-          { language: 'xml', label: 'XML' },
-          { language: 'yaml', label: 'YAML' }
-        ],
-      }
-    })
-    .then( editor => {
+  ClassicEditor.create(document.querySelector("#text"), {
+    codeBlock: {
+      languages: [
+        { language: "plaintext", label: "Plain text" },
+        { language: "bash", label: "Bash" },
+        { language: "c", label: "C" },
+        { language: "console", label: "Shell session" },
+        { language: "cs", label: "C#" },
+        { language: "cpp", label: "C++" },
+        { language: "css", label: "CSS" },
+        { language: "diff", label: "Diff" },
+        { language: "elixir", label: "Elixir" },
+        { language: "erlang", label: "Erlang" },
+        { language: "go", label: "Go" },
+        { language: "html", label: "HTML" },
+        { language: "java", label: "Java" },
+        { language: "javascript", label: "JavaScript" },
+        { language: "json", label: "JSON" },
+        { language: "kotlin", label: "Kotlin" },
+        { language: "php", label: "PHP" },
+        { language: "python", label: "Python" },
+        { language: "ruby", label: "Ruby" },
+        { language: "typescript", label: "TypeScript" },
+        { language: "xml", label: "XML" },
+        { language: "yaml", label: "YAML" },
+      ],
+    },
+  })
+    .then((editor) => {
       Typecho.insertFileToEditor = (file, url, isImage) => {
         if (isImage) {
-          editor.execute('insertImage', {
-            source: [{src: url, alt: file}]
-          })
+          editor.execute("insertImage", {
+            source: [{ src: url, alt: file }],
+          });
         } else {
-          editor.model.change(writer => {
-            const insertPos = editor.model.document.selection.getFirstPosition();
-            writer.insertText(file, {linkHref: url}, insertPos);
-          })
+          editor.model.change((writer) => {
+            const insertPos =
+              editor.model.document.selection.getFirstPosition();
+            writer.insertText(file, { linkHref: url }, insertPos);
+          });
         }
-      }
-    } )
-    .catch(error => {
-      console.error(error);
+      };
     })
-})
+    .catch((error) => {
+      console.error(error);
+    });
+});
