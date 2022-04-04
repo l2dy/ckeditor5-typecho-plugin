880da7a5956e320867b65dde5fddcd0bdd7352cd
Zero King <l2dy@icloud.com>
2022-04-04 06:00:31 +0000

    Set custom language list

--- CKEditor/templates/ck_init.js
+++ CKEditor/templates/ck_init.js
@@ -1,6 +1,33 @@
 $(document).ready(() => {
   ClassicEditor
-    .create(document.querySelector('#text'))
+    .create(document.querySelector('#text'), {
+      codeBlock: {
+        languages: [
+          { language: 'plaintext', label: 'Plain text' },
+          { language: 'bash', label: 'Bash' },
+          { language: 'c', label: 'C' },
+          { language: 'console', label: 'Shell session' },
+          { language: 'cs', label: 'C#' },
+          { language: 'cpp', label: 'C++' },
+          { language: 'css', label: 'CSS' },
+          { language: 'diff', label: 'Diff' },
+          { language: 'elixir', label: 'Elixir' },
+          { language: 'erlang', label: 'Erlang' },
+          { language: 'go', label: 'Go' },
+          { language: 'html', label: 'HTML' },
+          { language: 'java', label: 'Java' },
+          { language: 'javascript', label: 'JavaScript' },
+          { language: 'json', label: 'JSON' },
+          { language: 'kotlin', label: 'Kotlin' },
+          { language: 'php', label: 'PHP' },
+          { language: 'python', label: 'Python' },
+          { language: 'ruby', label: 'Ruby' },
+          { language: 'typescript', label: 'TypeScript' },
+          { language: 'xml', label: 'XML' },
+          { language: 'yaml', label: 'YAML' }
+        ],
+      }
+    })
     .then( editor => {
       Typecho.insertFileToEditor = (file, url, isImage) => {
         if (isImage) {
